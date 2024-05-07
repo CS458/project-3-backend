b79fb5c253e07e34607848868453c449e4915c32
sahandmoslemi <sahandmoslemi@gmail.com>
2024-05-07 20:41:35 +0300

    feat: Adds test 4.2.3 backend features.

--- controllers.py
+++ controllers.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect, url_for, jsonify
+from flask import render_template, redirect, url_for, jsonify, request
 from settings.app import app
+from models import User
 
 
 @app.route("/", methods=["GET"])
@@ -8,7 +9,13 @@ def login_page():
 
 @app.route("/", methods=["POST"])
 def login_page_post():
-    return redirect("http://localhost:3000")
+
+    user = User(request.form["identifier"])
+
+    if user.verify(request.form["password"]):
+        return redirect("http://localhost:3000")
+    
+    return redirect(url_for("login_page"))
 
 @app.route("/success")
 def success_page():
--- models.py
+++ models.py
@@ -1,3 +1,36 @@
+import os 
+import re
+from settings.config import DATABASE_DIR
+import json
 
 
+class User():
+    def __init__(self, identifier):
+        self.phone = self.email = None
+        phone_match = re.match(r'^(\+\d{1,2}\s?)?((\(\d{3}\))|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$', identifier)
+        email_match = re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', identifier)
+        
+        if phone_match:
+            self.phone = identifier
+        
+        elif email_match:
+            self.email = identifier
+
+    def verify(self, not_hashed_password):
+        with open(os.path.join(DATABASE_DIR, "user.json")) as file:
+            users = json.load(file)
+
+        if self.email:
+            for user in users:
+                if user["email"] == self.email:
+                    if user["password"] == not_hashed_password:
+                        return True
+                
+        elif self.phone:
+            for user in users:
+                if user["phone"] == self.phone:
+                    if user["password"] == not_hashed_password:
+                        return True
+        
+        return False
 
